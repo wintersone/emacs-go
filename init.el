9aadbec707d7d12bd9005734958bb991ad7350b6
Huang Huan <wintersone@gmail.com>
2016-07-02 01:01:20 +0800

    add org config

--- init.el
+++ init.el
@@ -1,4 +1,6 @@
+;;; package:
 (require 'package)
+(add-to-list 'default-frame-alist '(font . "PragmataPro-14" ))
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
@@ -50,3 +52,18 @@
                      (sanityinc/time-subtract-millis after-init-time before-init-time))))
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ lisp/init-org.el
@@ -0,0 +1,26 @@
+;;; Config for org
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+
+(setq org-agenda-files (quote ("/Users/huanghuan/Dropbox/daily.org")))
+(setq org-log-done t)
+;;set priority range from A to C with default A
+(setq org-highest-priority ?A)
+(setq org-lowest-priority ?C)
+(setq org-default-priority ?A)
+
+;;set colours for priorities
+(setq org-priority-faces '((?A . (:foreground "#F0DFAF" :weight bold))
+                           (?B . (:foreground "LightSteelBlue"))
+                           (?C . (:foreground "OliveDrab"))))
+
+;;open agenda in current window
+(setq org-agenda-window-setup (quote current-window))
+
+;;capture todo items using C-c c t
+(define-key global-map (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("t" "todo" entry (file+headline "/Users/huanghuan/Dropbox/daily.org" "Tasks")
+                  "* TODO [#A] %?")))
+(provide 'init-org)
